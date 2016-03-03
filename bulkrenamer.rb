96321785ac98a1172738419fbf453d83f86ab09a
kk_Ataka <kk_ataka@ring.skr.jp>
2016-03-03 21:29:55 +0900

    bump version

--- bulkrenamer.rb
+++ bulkrenamer.rb
@@ -2,7 +2,7 @@ class Bulkrenamer < Formula
   desc "BulkRenamer"
   homepage "https://github.com/gosyujin/bulkrenamer"
   head "https://github.com/gosyujin/homebrew-bulkrenamer.git"
-  version "v0.1"
+  version "v0.2"
   url "#{homepage}/releases/download/#{version}/bulkrenamer"
   sha1 "5e3ac6469395aae60a00c61f45b5343207cca330"
 
